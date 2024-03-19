1789dc54c9253434107e1288be95f9fc140325e8
蔡希隆 <caixilong@sainozomitakashis-MacBook-Air.local>
2024-03-19 01:18:16 -0700

    first ver.

---
+++ model.py
@@ -0,0 +1,63 @@
+# backend/server.py
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import base64
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/generate_plot', methods=['POST'])
+def generate_plot():
+    data = request.json
+    V = float(data['x'])
+    T = float(data['y'])
+
+    def obj2():     
+        nonlocal V
+        nonlocal T        
+        x = np.array([i / 10 for i in range(40)])
+        K= -572.872341576*V - 0.89982819062*T + 801.704037275 + 354.67789388*V**2
+        B= -0.33631875971*V - 0.0559766169332*T + 4.9467224951
+        Q = 4000
+        nu = 0.3
+        y2 = K/(1 + Q * np.exp(-B*x))**nu
+        return y2
+
+
+    x = np.array([i / 10 for i in range(40)])
+    plt.figure(figsize=(8,8), dpi= 100)
+    plt.plot(x, obj2(), '-', color = 'blue', label = 'predict curve', linewidth = 3.0)
+    plt.legend(fontsize = 24)
+    plt.title(f'V={V}L/min T={T}℃', fontsize = 30)
+    plt.xlabel('Pressure/MPa', fontsize = 30)
+    plt.ylabel('Capacity/L', fontsize = 30)
+
+    plt.tick_params(labelsize = 15)
+
+
+
+    ax = plt.gca()
+    ax.spines['top'].set_linewidth(2.0)
+    ax.spines['bottom'].set_linewidth(2.0)
+    ax.spines['left'].set_linewidth(2.0)
+    ax.spines['right'].set_linewidth(2.0)
+    
+    
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_base64 = base64.b64encode(buffer.getvalue()).decode()
+    
+    # Close plot to prevent resource leaks
+    plt.close()
+    
+    # Return the base64-encoded image
+    return jsonify({'plotBase64': image_base64})
+
+if __name__ == '__main__':
+    app.run(debug=True)
